8011bb65999ab4c796785d45079632e029001649
megxn <megan.mcmullin@dcmail.ca>
2026-07-28 15:26:17 -0400

    Comment header
    Added comment header, html formatting, and connected html to js and css

--- lab4/part2/js/main.js
+++ lab4/part2/js/main.js
@@ -1,3 +1,9 @@
+    /*
+    Name: Megan McMullin
+    File: main.js
+    Date: July 28, 2026
+    INFT 1206: Lab 4 Part 2: Image Gallary
+    */
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
 
