d30457bdb6818ac92c6664158ded4df8e849fded
megxn <megan.mcmullin@dcmail.ca>
2026-07-28 17:29:47 -0400

    Updated collisionDetect() method
    Updated collisionDetect() method

--- lab4/part4/js/main-finished.js
+++ lab4/part4/js/main-finished.js
@@ -140,6 +140,20 @@ class EvilCircle extends Shape {
       this.velY += this.velY; // bounce back from top edge
     }
   }
+  // adding collision detecting 
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+        }
+      }
+    }
+  }
 }
 
 // Animating the ball
