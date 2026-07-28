c4a7780b784ea407890c55cfc0fd1564a314d7a8
megxn <megan.mcmullin@dcmail.ca>
2026-07-28 15:46:46 -0400

    Add the images to the thumbnail bar
    Add the images to the thumbnail bar

--- lab4/part2/js/main.js
+++ lab4/part2/js/main.js
@@ -24,3 +24,28 @@ const images = [
 
 // baseURl constant containing the images
 const baseURL = "./images/";
+
+// create a for...of loop to loop through the images 
+// for each image, create a new <img> element with src and alt.
+
+for (const image of images) {
+  // Create a new image element
+  const newImage = document.createElement("img");
+  // Set the src and alt text for the image
+  newImage.src = `${baseURL}${image.filename}`;
+  newImage.alt= image.alt;
+  // Allow the image to be focusable via the keyboard
+  newImage.tabIndex = "0";
+  // Append the image as a child of the thumbBar
+  thumbBar.appendChild(newImage);
+  // Update the display to show the image full size when a
+  // thumb is clicked
+  newImage.addEventListener("click", updateDisplayedImage);
+  // Update the display to show the image full size when the 
+  // "Enter" key is pressed after it has been focused
+  newImage.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+      updateDisplayedImage(e);
+    }
+  });
+}
