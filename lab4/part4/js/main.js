f03e5c291bd6c34c2a24e1a699b751e25552ff7d
megxn <megan.mcmullin@dcmail.ca>
2026-07-29 19:14:40 -0400

    Updated main.js name
    Updated main.js name

--- lab4/part4/js/main.js
+++ lab4/part4/js/main.js
@@ -1,6 +1,6 @@
     /*
     Name: Megan McMullin
-    File: main-finished.js
+    File: main.js
     Date: July 28, 2026
     INFT 1206: Lab 4 Part 4: Adding features to our bouncing balls demo
     */
